cc5abb2b77bcbbc2297bccd08c9d56bb5f94a778
Billy Rhoades <Aaron5367@knightsofreason.net>
2010-06-18 14:01:12 -0500

    Move more stuff into functions and whatnot...

--- moobot.php
+++ moobot.php
@@ -28,9 +28,9 @@ debug_message( "Inital start of Moobot" );
 //$dbcnx = mysql_connect( "localhost", $CONFIG[dbuser], $CONFIG[dbpass] );
 //mysql_select_db( $CONFIG[dbname], $dbcnx );
 $con = array();
-$alive = "TRUE";
+$alive = TRUE;
 
-while( $alive == "TRUE" )
+while( $alive == TRUE )
 {
   $bstatus['botstarttime'] = time();
   init();
@@ -39,27 +39,11 @@ while( $alive == "TRUE" )
 
 function init()
 {
-  global $con, $CONFIG, $servers, $other, $bot, $bstatus, $commandtree, $commands; 
+  global $con, $CONFIG, $servers, $other, $bot, $bstatus, $commandtree, $commands, $lasttime; 
   
   debug_message( "Entering main moobot function." );
   $firsttime = TRUE;
-  if( is_int( $CONFIG[server] ) )
-  {
-    if( $CONFIG[server] == 0 )
-      $CONFIG[server] = "irc.freenode.net";
-    else if( $CONFIG[server] == 1 )
-      $CONFIG[server] = "irc.quakenet.org";
-  }
-  $old = $CONFIG[server];
-  $CONFIG[server] = gethostbyname( $CONFIG[server] );
-  debug_message( "Resolved $old to ".$CONFIG[server] );
-  $con['socket'] = fsockopen( $CONFIG[server], $CONFIG[port], $errno, $errstr, 1 );
-  stream_set_blocking( $con['socket'], 0 );
-  stream_set_timeout( $con['socket'], 100 );
-	$lasttime = time();
-  usleep( $CONFIG[sleeptime] );
-  cmd_send( "USER ". $CONFIG[nick] ." ".$CONFIG[vhost]." ".$CONFIG[vhost]." :". $CONFIG[name], FALSE, TRUE );
-  cmd_send( "NICK ". $CONFIG[nick] ." ".$CONFIG[vhost], FALSE, TRUE );
+  connect_to_irc( );
   debug_message( "Connected to main server, starting socket read loop." );
   while( !feof( $con['socket'] ) )
   {
@@ -75,12 +59,6 @@ function init()
       print date("[ m/d/y @ H:i:s ]")."<- ".$con['buffer']['all'] ."\n";
 
     $bstatus['lines']++;
-
-    if( substr( $con['buffer']['all'], 0, 6 ) == 'PING :' )
-    {
-      cmd_send( 'PONG :'.substr( $con['buffer']['all'], 6 ), FALSE, TRUE );
-      $lasttime = time();
-    }
     
     if( $firsttime == TRUE && ( stripos( $con['buffer']['all'], "/motd" ) !== FALSE 
         || stripos( $con['buffer']['all'], "MOTD File" ) !== FALSE )
@@ -106,17 +84,8 @@ function init()
       }
       $firsttime = FALSE;
       if( $CONFIG[nickpass] != NULL )
-      {
-        if( $CONFIG[nickserv] == "NickServ" || $CONFIG[nickserv] == "Q@CServe.quakenet.org" )
-        {
-          if( $CONFIG[server] == 0 || $CONFIG[nickserv] == "NickServ" )
-            cmd_send( "PRIVMSG NickServ :IDENTIFY ".$CONFIG[nick]." ".$CONFIG[nickpass], TRUE, TRUE );
-          else if( $CONFIG[server] == 1 || $CONFIG[nickserv] == "Q@CServe.quakenet.org" )
-            cmd_send( "PRIVMSG Q@CServe.quakenet.org :AUTH ".$CONFIG[nick]." ".$CONFIG[nickpass], TRUE, TRUE );
-        }
-        else
-          cmd_send( "PRIVMSG ".$CONFIG[nickserv]." :AUTH ".$CONFIG[nick]." ".$CONFIG[nickpass], TRUE, TRUE );
-      }
+        identify( );
+        
       //cmd_send( "MODE ".$CONFIG[nick]." +x " );
     }
 
@@ -245,73 +214,6 @@ function init()
       if( !$eval && $command != NULL )
         talk( $channel, "%$command is not known, try using %help." );
     }
-    /*else if( stripos( $text, " is " ) !== FALSE || stripos( $text, " are " ) !== FALSE )
-    {
-      if( stripos( $text, " is " ) )
-      {
-        $statement = explode( " is ", $text );
-        $verb = "is";
-      }
-      else
-      {
-        $statement = explode( " are ", $text );
-        $verb = "are";
-      }
-      
-      //Check to see if it has been said before...
-      $test = mysql_fetch_array( mysql_query( "SELECT * FROM state WHERE verb=\"".mysql_escape_string($verb)." AND this=\"".mysql_escape_string($this)."\" AND that=\"".mysql_escape_string($that)."\" " ) );
-      //If it has, then add to the times it has been said
-      if( count( $test ) > 0 )
-      {
-        $test['timessaid']++;
-        mysql_query( "UPDATE state SET timessaid=".$test['timessaid']." WHERE verb=\"".mysql_escape_string($verb)." AND this=\"".mysql_escape_string($this)."\" AND that=\"".mysql_escape_string($that)."\" " );
-      }
-      else         //If it hasn't add it in
-      mysql_query( "INSERT INTO state (\"this\",\"verb\",\"that\",\"whostarted\") VALUES (\"".mysql_escape_string($statement[0])."\",\"".$verb."\",\"".mysql_escape_string($statement[1])."\",\"".mysql_escape_string($name)."\")");
-    }
-    else if( preg_match( "/?/", $text ) )
-    {
-      $text = rtrim( $text, "?" );
-      $count = 0;
-      while( $row = mysql_fetch_array( $query ) )
-      {
-        if( count( $row ) < 2 )
-          break;
-        $response[$count] = $row;
-        $count++;
-      }
-      if( count( $response ) > 0 )
-      {
-        for( $i=0; $i<count($response); $i++ )
-        {
-          $cur = $response[$i];
-          if( $cur['timessaid'] > $top['stated'] )
-          {
-            $top['stated'] = $cur['timessaid'];
-            $top['id'] = $i;
-          }
-        }
-        //Okay, we know the top and now we are going to update the times quiered and say it!
-        talk( $channel, "I think ".$top['this']." ".$top['verb']." ".$top['that'] );
-        
-        $top['quieries']++;
-        mysql_query( "UPDATE state SET queries=".$top['queries']." WHERE verb=\"".$top['verb']." AND this=\"".$top['this']."\" AND that=\"".$top['that']."\" " );
-      }
-      
-    }*/
-    /*else if( stripos($con['buffer']['all'], " :", 10 ) !== FALSE )
-    {
-      $start = strpos( $con['buffer']['all'], ":", 1 )+1;
-      $text = sanitize2(sanitize(substr( $con['buffer']['all'], $start )));
-      $time = time();
-      if( $channels[$chanid]['log'] == "TRUE" )
-      {
-        $entryid = mysql_num_rows( mysql_query( "SELECT * FROM log" ) );
-        mysql_query("INSERT INTO log (user,text,channel,time,lineid) VALUES (\"$name\",\"$text\",\"$channel\",\"$time\",\"$entryid\")");
-      }
-      else if( $channels[$chanid]['log'] == "FALSE" )
-        print "Channel has disabled log submission.\r\n"; 
-     } */
      else if( stripos( $con['buffer']['all'], "(Nick collision from services.)" ) !== FALSE )
      {
        debug_message( "Dying, nick collision.\r\n", TRUE );
@@ -437,5 +339,4 @@ function init()
     usleep( $CONFIG[sleeptime]*1000 );
   }
 }
-
 ?>
